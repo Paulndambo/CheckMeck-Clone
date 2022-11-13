96fc60e69dc2ef4220d3f164d2c5ffdc2b032e41
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2022-11-13 09:32:02 +0300

    APIs updated

--- CheckMechBackend/apps/drivers/serializers.py
+++ CheckMechBackend/apps/drivers/serializers.py
@@ -1,14 +1,35 @@
 from rest_framework import serializers
 from .models import Driver, Car
-
-class CarSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Car
-        fields = "__all__"
+from django.contrib.auth.models import User
 
 
 class DriverSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Driver
-        fields = "__all__"
+        fields = ["first_name", "last_name", 
+                    "phone_number", "license_number", 
+                    "id_number", "kra_pin", "birth_date", "gender", 
+                    "marital_status", "postal_code", "town", "country", "user"]
 
+    def create(self, validated_data):
+        user_id = self.context['user_id']
+        user=User.objects.get(id=user_id)
+        return Driver.objects.create(user=user, **validated_data)
+
+    def get_user(self, obj):
+        return obj.user.id
+
+
+class CarSerializer(serializers.ModelSerializer):
+    driver = DriverSerializer(read_only=True)
+
+    class Meta:
+        model = Car
+        fields = ["number_plate", "car_color", "brand",
+                  "car_model", "year_manufactured", ""]
+
+    def create(self, validated_data):
+        user_id = self.context['user_id']
+        driver = Driver.objects.get(user_id=user_id)
+        return Car.objects.create(driver=driver, **validated_data)
